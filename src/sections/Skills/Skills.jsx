56ac9d9ddfe1fd27a0d85fc985d6a27cd75f1719
Jayanth <jayanth10aug@gmail.com>
2025-04-11 14:42:26 +0530

    Update Skills.jsx

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -13,20 +13,25 @@ function Skills() {
     <section id="skills" className={styles.container}>
       <h1 className="sectionTitle">Skills</h1>
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="React" />
         <SkillList src={checkMarkIcon} skill="HTML" />
         <SkillList src={checkMarkIcon} skill="CSS" />
+        <SkillList src={checkMarkIcon} skill="React" />
         <SkillList src={checkMarkIcon} skill="JavaScript" />
-        <SkillList src={checkMarkIcon} skill="Bootstrap" />
+        <SkillList src={checkMarkIcon} skill="Material UI" />
+        <SkillList src={checkMarkIcon} skill="Tailwind UI" />
       </div>
       <hr />
       <div className={styles.skillList}>
         <SkillList src={checkMarkIcon} skill="C" />
         <SkillList src={checkMarkIcon} skill="Java" />
-        <SkillList src={checkMarkIcon} skill="J2EE" />
+        <SkillList src={checkMarkIcon} skill="MySql" />
+        <SkillList src={checkMarkIcon} skill="Git" />
       </div>
       <hr />
       <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="Node.js" />
+        <SkillList src={checkMarkIcon} skill="Express.js" />
+        <SkillList src={checkMarkIcon} skill="MongoDB" />
         <SkillList src={checkMarkIcon} skill="Spring" />
         <SkillList src={checkMarkIcon} skill="Spring Boot" />
         <SkillList src={checkMarkIcon} skill="Microservices" />
